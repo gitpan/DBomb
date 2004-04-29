2338dc984b4fc86cb4ec2c93e34d065926be83ac
John Millaway <millaway@acm.org>
2004-04-29 20:23:57 +0000

    Import of MILLAWAY/DBomb-0.18 from CPAN.
    gitpan-cpan-distribution: DBomb
    gitpan-cpan-version:      0.18
    gitpan-cpan-path:         MILLAWAY/DBomb-0.18.tar.gz
    gitpan-cpan-author:       MILLAWAY
    gitpan-cpan-maturity:     released

---
+++ DBomb/Version.pm
@@ -0,0 +1,17 @@
+package DBomb::Version;
+
+=head1 NAME
+
+DBomb::Version - The DBomb Release Version
+
+=cut
+
+use strict;
+use warnings;
+
+## This is the RELEASE version, not the CVS Revision.
+our $VERSION = '0.18'; ## tag: version-0_18 (remember to update README)
+
+1;
+__END__
+
